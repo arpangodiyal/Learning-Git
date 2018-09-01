480eaf0b82b2684ac968689c799b5fe329872f88
Arpan Godiyal <arpangodiyal24@gmail.com>
2018-09-01 13:59:28 +0530

    Second commit

---
+++ Untitled2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string s1 = "sfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpm";
+	string s2 = "sfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpmsfrpm";
+	
+	if(s1 != s2)
+		cout << "Not equal";
+	else
+		cout << "Equal";
+}
